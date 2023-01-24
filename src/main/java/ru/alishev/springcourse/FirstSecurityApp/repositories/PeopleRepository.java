eab32657fa29436897a0d77859847a02154fccf0
yrkash <yrkash@gmail.com>
2023-01-25 01:13:11 +0300

    Init commit. Basic config for Authentication in Spring. Logic of Authentication realized in AuthProviderImpl

---
+++ src/main/java/ru/alishev/springcourse/FirstSecurityApp/repositories/PeopleRepository.java
@@ -0,0 +1,12 @@
+package ru.alishev.springcourse.FirstSecurityApp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.alishev.springcourse.FirstSecurityApp.models.Person;
+
+import java.util.Optional;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+    Optional<Person> findByUsername(String username);
+}
---
+++ src/main/java/ru/alishev/springcourse/FirstSecurityApp/security/PersonDetails.java
@@ -0,0 +1,57 @@
+package ru.alishev.springcourse.FirstSecurityApp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.alishev.springcourse.FirstSecurityApp.models.Person;
+
+import java.util.Collection;
+
+public class PersonDetails implements UserDetails {
+
+    private final Person person;
+
+    @Autowired
+    public PersonDetails(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return person.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return person.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+}
