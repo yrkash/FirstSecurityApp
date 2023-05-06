a9ee11f2a15be287ae95003e6f083d42b7b84449
yrkash <yrkash@gmail.com>
2023-05-06 11:39:52 +0300

    basic authorization with @PreAuthorize

--- src/main/java/ru/alishev/springcourse/FirstSecurityApp/controllers/HelloController.java
+++ src/main/java/ru/alishev/springcourse/FirstSecurityApp/controllers/HelloController.java
@@ -1,17 +1,25 @@
 package ru.alishev.springcourse.FirstSecurityApp.controllers;
 
 
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.alishev.springcourse.FirstSecurityApp.security.PersonDetails;
+import ru.alishev.springcourse.FirstSecurityApp.services.AdminService;
 
 
 @Controller
 public class HelloController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
         return "hello";
@@ -28,6 +36,7 @@ public class HelloController {
 
     @GetMapping("/admin")
     public String adminPage() {
+        adminService.doAdminStuff();
         return "admin";
     }
 }
