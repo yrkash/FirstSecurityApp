4e9bf1c2acf1cbaf92f512e1bb32813fab406e75
yrkash <yrkash@gmail.com>
2023-06-04 23:10:47 +0300

    First step in JWT authentication

--- src/main/java/ru/alishev/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/alishev/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -28,7 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //конфигурируем сам Spring Security
         //конфигурируем авторизацию
-        http.authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/auth/login","/auth/registration","/error").permitAll()
 //                .antMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().hasAnyRole("ADMIN","USER")
--- src/main/java/ru/alishev/springcourse/FirstSecurityApp/controllers/AuthController.java
+++ src/main/java/ru/alishev/springcourse/FirstSecurityApp/controllers/AuthController.java
@@ -1,28 +1,34 @@
 package ru.alishev.springcourse.FirstSecurityApp.controllers;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.alishev.springcourse.FirstSecurityApp.dto.PersonDTO;
 import ru.alishev.springcourse.FirstSecurityApp.models.Person;
+import ru.alishev.springcourse.FirstSecurityApp.security.JWTUtil;
 import ru.alishev.springcourse.FirstSecurityApp.services.RegistrationService;
 import ru.alishev.springcourse.FirstSecurityApp.utils.PersonValidator;
 
 import javax.validation.Valid;
+import java.util.Collections;
+import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final PersonValidator personValidator;
     private final RegistrationService registrationService;
+    private final JWTUtil jwtUtil;
+    private final ModelMapper modelMapper;
     @Autowired
-    public AuthController(PersonValidator personValidator, RegistrationService registrationService) {
+    public AuthController(PersonValidator personValidator, RegistrationService registrationService, JWTUtil jwtUtil, ModelMapper modelMapper) {
         this.personValidator = personValidator;
         this.registrationService = registrationService;
+        this.jwtUtil = jwtUtil;
+        this.modelMapper = modelMapper;
     }
 
     @RequestMapping("/login")
@@ -35,13 +41,20 @@ public class AuthController {
         return "auth/registration";
     }
     @PostMapping("/registration")
-    public String performRegistration(@ModelAttribute("person") @Valid Person person,
-                                      BindingResult bindingResult) {
+    public Map<String, String> performRegistration(@RequestBody @Valid PersonDTO personDTO,
+                                                   BindingResult bindingResult) {
+        Person person =convertToPerson(personDTO);
+
         personValidator.validate(person, bindingResult);
         if (bindingResult.hasErrors())
-            return "/auth/registration";
+            return Map.of("message", "Error!");
         registrationService.register(person);
-        return "redirect:/auth/login";
+        String token = jwtUtil.generateToken(person.getUsername());
+        return Map.of("jwt-token", token);
+    }
+
+    public Person convertToPerson(PersonDTO personDTO) {
+        return this.modelMapper.map(personDTO, Person.class);
     }
 
 }
